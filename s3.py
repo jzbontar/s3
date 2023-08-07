45cff182ac9a733b21ddb6488a686deb0117c08a
Jure Zbontar <jure.zbontar@gmail.com>
2023-08-07 08:58:10 +0200

    improve ls for dirs

--- s3.py
+++ s3.py
@@ -1,18 +1,18 @@
 import argparse
 import configparser
-from pathlib import Path
+import os
 from subprocess import run
 
 parser = argparse.ArgumentParser()
 parser.add_argument("action")
-parser.add_argument("path", type=Path)
+parser.add_argument("path")
 args, unknownargs = parser.parse_known_args()
 
 config = configparser.ConfigParser()
-config.read(Path.home() / ".s3")
+config.read(os.path.expanduser("~") + "/.s3")
 
 cmd_prefix = f"aws s3 --endpoint-url={config['default']['endpoint_url']} "
-s3path = config["default"]["bucket"] + str(args.path.resolve())
+s3path = config["default"]["bucket"] + os.path.abspath(args.path)
 
 
 def isdir(s3path):
@@ -27,7 +27,7 @@ def main():
         else:
             cmd = cmd_prefix + f"cp {s3path} {args.path}"
     elif args.action == "upload":
-        if args.path.is_dir():
+        if os.path.isdir(args.path):
             cmd = cmd_prefix + f"sync {args.path} {s3path}"
         else:
             cmd = cmd_prefix + f"cp {args.path} {s3path}"
@@ -35,5 +35,8 @@ def main():
         cmd = cmd_prefix + f"rm {s3path}" + (" --recursive" if isdir(s3path) else "")
     elif args.action == "ls":
         cmd = cmd_prefix + f"ls {s3path}"
+        if args.path.endswith("/"):
+            cmd += "/"
+        print(s3path)
 
     run(cmd.split() + unknownargs)
